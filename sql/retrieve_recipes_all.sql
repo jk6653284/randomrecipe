46e8e7edea60411e5f3c626f6872729231a7fc7f
jk6653284 <jk6653284@gmail.com>
2021-01-25 12:13:03 +0100

    Change random selection so it's equally likely to get a recipe from each source

--- sql/retrieve_recipes_all.sql
+++ sql/retrieve_recipes_all.sql
@@ -1,4 +1,12 @@
-SELECT *
-FROM {TABLE_NAME}
-ORDER BY RANDOM()
-LIMIT {RETRIEVE_NUM};
+/*Weighted randomization*/
+with randomized as (
+select
+	{TABLE_NAME}.*
+	,row_number() over (partition by website_name order by random()) rn
+from {TABLE_NAME}
+)
+select *
+from randomized
+where rn = 1
+order by random()
+limit {RETRIEVE_NUM};
